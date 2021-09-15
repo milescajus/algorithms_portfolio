d87e2f76f5b2f22d4b97080e73153133c27b1f9d
milescajus <80205080+milescajus@users.noreply.github.com>
2021-09-15 15:56:00 -0500

    added comments

--- WK2_A1.cs
+++ WK2_A1.cs
@@ -2,13 +2,13 @@ using System;
 
 class Week2
 {
-	// O(1)
+	// O(1) as this always returns the first item, which is not affected by the length of the input.
 	public int ConstantTime (int[] a)
 	{
 		return a[0];
 	}
 
-	// O(n)
+	// O(n) as the time taken scales linearly with the input integer n.
 	public void LinearTime (int n)
 	{
 		for (int i = 0; i < n; i++)
@@ -17,7 +17,7 @@ class Week2
 		}
 	}
 
-	// O(n^2)
+	// O(n^2) as this is a nested loop that takes n*n time, i.e. n^2, growing quadratically as n grows.
 	public void QuadraticTime (int n)
 	{
 		for (int i = 0; i < n; i++)
