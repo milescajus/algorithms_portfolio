d619f9d41411bbcde3c749031ef26c592ff26744
milescajus <80205080+milescajus@users.noreply.github.com>
2021-12-12 00:07:41 +0100

    refactored to OOP structure, implemented ultimate print

--- final/ttt.py
+++ final/ttt.py
@@ -1,21 +1,47 @@
-grid = [" " for i in range(9)]
-did_win = ""
-turn = True
-player = ["\033[91mO\033[0m", "\033[92mX\033[0m"]
-while did_win == "":
-    for i in range(len(grid)):
-        print("[" + grid[i] + "]", end="\n" if i % 3 == 2 else "")							# print grid
-    user = int(input(": "))
-    if user in range(len(grid)) and grid[user] == " ":
-        grid[user] = player[1 if turn else 0]						                        # check if move is valid and place tile
-    turn = not turn																			# change turns
-    for seq in [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                [0, 3, 6], [1, 4, 7], [2, 5, 8],
-                [0, 4, 8], [2, 4, 6]]:
-        if len(set([grid[seq[i]] for i in range(len(seq))])) == 1 and grid[seq[0]] != " ":
-            did_win = grid[seq[0]]                                                          # check for wins
+class Game:
+    def __init__(self):
+        # self.grid = [" " for i in range(9)]
+        self.grid = [str(i) for i in range(9)]
+        self.did_win = ""
+        self.turn = True
+        self.player = ["\033[91mO\033[0m", "\033[92mX\033[0m"]
 
-# print final grid
-for i in range(len(grid)):
-    print("[" + grid[i] + "]", end="\n" if i % 3 == 2 else "")
-print(did_win)
+    def play(self):
+        while self.did_win == "":
+            self.print()
+            user = int(input(": "))
+            if user in range(len(self.grid)) and self.grid[user] == " ":
+                self.grid[user] = self.player[1 if self.turn else 0]						            # check if move is valid and place tile
+            self.turn = not self.turn																	# change turns
+            for seq in [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+                        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                        [0, 4, 8], [2, 4, 6]]:
+                if len(set([self.grid[seq[i]] for i in range(len(seq))])) == 1 and self.grid[seq[0]] != " ":
+                    self.did_win = self.grid[seq[0]]                                                    # check for wins
+
+        # print final grid
+        for i in range(len(self.grid)):
+            print("[" + self.grid[i] + "]", end="\n" if i % 3 == 2 else "")
+        print(self.did_win)
+        return self.player.index(self.did_win)
+
+    def print(self):
+        for i in range(len(self.grid)):
+            print("[" + self.grid[i] + "]", end="\n" if i % 3 == 2 else "")							# print self.grid
+
+
+class Ultimate:
+    def __init__(self):
+        self.metagrid = [Game() for i in range(9)]
+
+    def print(self):
+        splits = ((0, 3), (3, 6), (6, 9))
+
+        for s in splits:
+            for ss in splits:
+                for g in self.metagrid[s[0]:s[1]]:
+                    for e in g.grid[ss[0]:ss[1]]:
+                        print('[' + e + ']', end='')
+                    print('\t', end='')
+                print()
+            print()
