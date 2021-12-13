91ee1dcae4870262e7ee5210eb10b1925ec32d4b
milescajus <80205080+milescajus@users.noreply.github.com>
2021-12-13 15:47:43 +0100

    implemented single- and multiplayer mode option, AI opponent is not I

--- final/ttt.py
+++ final/ttt.py
@@ -1,11 +1,22 @@
+from random import randrange
+
+
 class Game:
-    def __init__(self):
+    def __init__(self, multiplayer=True):
         self.grid = [" " for i in range(9)]
+        self.player = ["\033[91mO\033[0m", "\033[92mX\033[0m"]
+        self.multiplayer = multiplayer
 
     def make_move(self, player):
-        move = int(input("Make a move (" + player + "): "))
-        while not (move in range(len(self.grid)) and self.grid[move] == " "):
-            move = int(input("Invalid move, try again: "))
+        move = -1
+        if player == self.player[0] and not self.multiplayer:
+            # AI CODE (TODO: REPLACE RANDOM APPROACH)
+            while not (move in range(len(self.grid)) and self.grid[move] == " "):
+                move = randrange(len(self.grid))
+        else:
+            move = int(input("Make a move: "))
+            while not (move in range(len(self.grid)) and self.grid[move] == " "):
+                move = int(input("Invalid move, try again: "))
         self.grid[move] = player
         if self.check_win():
             self.grid = [player for i in range(9)]
@@ -28,13 +39,17 @@ class Game:
 
 class Ultimate:
     def __init__(self):
-        self.game_grid = [Game() for i in range(9)]
+        print("\033[2J\033[H", end='')  # clear screen
+        self.multiplayer = True if int(input("[1] Single-player\n[2] Multi-player\n: ")) == 2 else False
+        self.game_grid = [Game(self.multiplayer) for i in range(9)]
         self.turn = True
         self.player = ["\033[91mO\033[0m", "\033[92mX\033[0m"]
         self.playfield = 0
 
     def print(self, fields=False):
         print("\033[2J\033[H", end='')  # clear screen
+        print("Mode:", "Multi-player" if self.multiplayer else "Single-player")
+        print("Turn:", self.player[self.turn], '\n')
 
         splits = ((0, 3), (3, 6), (6, 9))
 
@@ -71,15 +86,16 @@ class Ultimate:
         # main game loop
         while not self.check_win():
             self.print()
-            self.playfield = self.game_grid[self.playfield].make_move(self.player[self.turn])
 
             if len(set(self.game_grid[self.playfield].grid)) == 1 and self.game_grid[self.playfield].grid[0] != " ":
                 # next move is at 'full' game, player gets to choose next field
                 self.playfield = self.get_valid_playfield()
 
+            self.playfield = self.game_grid[self.playfield].make_move(self.player[self.turn])
             self.turn = not self.turn
 
-        print("Congratulations", self.player[self.turn] + '!')
+        self.print()
+        print("Congratulations", self.player[not self.turn] + '!')  # congratulate the previous player as they actually won
 
     def check_win(self):
         for seq in [[0, 1, 2], [3, 4, 5], [6, 7, 8],
